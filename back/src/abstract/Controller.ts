18cfdc5a81ee4df8380c9e8d2c72f3482481c5d3
DanielTseng2004 <112913131+DanielTseng2004@users.noreply.github.com>
2025-03-12 11:45:00 +0800

    0311

---
+++ back/src/abstract/Controller.ts
@@ -0,0 +1,10 @@
+
+import {Request, Response} from "express";
+import { Service } from "./Service";
+
+export abstract class Controller{
+    protected abstract service:Service;
+    constructor(){
+        
+    }
+}
